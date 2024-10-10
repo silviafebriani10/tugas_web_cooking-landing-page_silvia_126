16710a0a74484e8ab4ceb1666bb8c657410ab5cb
Silvia Febriani <119495786+silviafebriani10@users.noreply.github.com>
2024-10-10 20:30:34 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Interaksi sederhana dengan JavaScript
+    const heroImage = document.querySelector('img[alt="Gambar Hero"]');
+    heroImage.addEventListener('click', function() {
+        alert('Selamat datang di Resep Favorit Silvia!');
+    });
+});
